d4f87b8fd87359407c87224a35e85ca912394e06
Jeremy Hamley <jeremyhamley@gmail.com>
2021-02-07 08:41:11 -0500

    branch page

---
+++ static/js/branch.js
@@ -0,0 +1,185 @@
+
+// create a function to unpack data
+function unpack(rows, index) {
+    return rows.map(function (row) {
+        return row[index];
+    });
+}
+
+
+// read data, collect ids, and load the ids into the dropdown  //
+
+d3.json("samples.json").then(function (data) {
+    var ids = unpack(data.metadata, "id");
+    d3.select("#selDataset")
+        .selectAll("select")
+        .data(ids)
+        .enter()
+        .append("option")
+        .html(function (d) {
+            return `<option value ="${d}">${d}</option>`;
+        });
+
+
+    console.log(data);
+    updatePlotly();
+});
+
+// ----------------------------------------------------  //
+
+
+
+
+// ----------------------------------------------------  //
+
+// Create and Call function:  updatePlotly() when a change takes place to the DOM //
+
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
+    console.log(dataset)
+
+
+    // ----------------------------------------------------  //
+    //  Pull data for the id selected in the dropdown //
+
+    d3.json("samples.json").then((data) => {
+        var person_id = data.samples.find(({ id }) => id === dataset);
+        console.log(person_id);
+        // store the top 10 values for the bar chart
+        var otu_ids_sliced = person_id.otu_ids.slice(0, 10);
+        console.log(otu_ids_sliced);
+
+        var values_sliced = person_id.sample_values.slice(0, 10);
+        console.log(values_sliced);
+
+        var labels_sliced = person_id.otu_labels.slice(0, 10);
+        // console.log(labels_sliced);
+
+
+
+        // ----------------------------------------------------  //
+        //  Create the bar chart //
+
+        var y = otu_ids_sliced.map(function (a) { return "OTU ID " + a; });
+        var x = values_sliced.sort((a, b) => a - b);
+        var labels = labels_sliced;
+
+        var trace1 = {
+            x: x,
+            y: y,
+            type: "bar",
+            text: labels,
+            orientation: "h"
+        };
+
+        // Create the data array for the plot
+        var data1 = [trace1];
+
+        // Define the plot layout
+        var layout = {
+            title: "Top 10 OTUs (Operational Taxonomic Units)",
+            xaxis: { title: "Microbe Count" },
+            barmode: "group"
+        };
+
+        // Plot the chart to a div tag with id "bar"
+        Plotly.newPlot("bar", data1, layout);
+
+
+
+
+        // ---------------------------------------------------- //
+        // Collect data for Bubble chart //
+
+
+        var otu_ids = person_id.otu_ids;
+        var values = person_id.sample_values;
+        var labels = person_id.otu_labels;
+
+        var x2 = otu_ids;
+        var y2 = values;
+        var labels2 = labels;
+
+        var trace2 = {
+            x: x2,
+            y: y2,
+            mode: "markers",
+            text: labels2,
+            marker: {
+                color: otu_ids,
+                size: values
+            }
+        };
+
+        // ---------------------------------------------------- //
+        // Create Bubble chart //
+
+        var data2 = [trace2];
+        // Define the plot layout
+        var layout2 = {
+            title: "OTUs (Operational Taxonomic Units)",
+            xaxis: { title: "OTU ID" },
+        };
+        // Plot the chart to a div tag with id "bubble"
+        Plotly.newPlot("bubble", data2, layout2);
+
+    });
+
+
+    // ---------------------------------------------------- //
+    // Collect and display data for Demographic Info card
+
+    dem_dataset = parseInt(dataset)
+    d3.json("samples.json").then((data) => {
+        var dem_data = data.metadata.find(({ id }) => id === dem_dataset);
+
+        var indv_dem_data = d3.select("#sample-metadata");
+        indv_dem_data.text("");
+        Object.entries(dem_data).forEach((key) => {   
+            indv_dem_data.append("h4").text(key[0] + ": " + key[1]);    
+        });
+
+
+
+        //  ----------------------------------------------------  //
+
+        // Add the Gauge Chart
+
+        // add var for wash frequency gauge
+        var wfreq = dem_data.wfreq;
+        console.log(wfreq)
+
+        var trace3 = {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wfreq,
+            title: { text: "Belly Button Scrubs per Week" },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: {range: [null,9], tickwidth: 5},
+                bar: { color: "yellow" },
+                steps: [
+                    { range: [0,2], color: "greenyellow"},
+                    { range: [2,4], color: "lightgreen"},
+                    { range: [4,6], color: "green"},
+                    { range: [6,8], color: "darkgreen"},
+                    { range: [8,9], color: "greenblue"},
+                ]
+
+            }
+        };
+        // Create the data array for the plot
+        var data3 = [trace3];
+
+        // Plot the chart to a div tag with id "gauge"
+        Plotly.newPlot("gauge", data3);
+
+    });
+  
+};
---
+++ static/js/logic.js
@@ -0,0 +1,138 @@
+
+// Create map object
+var myMap2 = L.map("map2", {
+    center: [33.8300, -84.3847],
+    zoom: 5
+
+  });
+  
+
+// Add tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap2);
+
+
+// Import Data
+d3.csv("data/2020_rollins_loc_sum.csv").then(function(data) {
+
+  // convert strings to int
+  data.forEach(function(data) {
+    data.lat = +data.lat;
+    data.lon = +data.lon;
+  });
+
+      // Loop through the rollins data
+      for (var i = 0; i < data.length; i++) {
+
+        var color = "";
+        if (data[i].brand == "Orkin") {
+          color = "#a60707";
+        }
+        else if (data[i].brand == "CC Franchise") {
+          color = "#ebe71e";
+        }
+        else if (data[i].brand == "Rollins") {
+            color = "#a60707";
+          }
+        else {
+          color = "#0df005";
+        }
+        
+        var branchLocs = [data[i].lat,data[i].lon]
+
+        // Add circles to map
+        // console.log(data[i].TOTALACRES);
+        // console.log(droughtLocs);
+        L.circle(branchLocs, {
+          fillOpacity: 0.7,
+          color: "black",
+          weight: 0.5,
+          fillColor: color,
+          // Adjust radius
+          radius: data[i].sum_promo_sales * 15,
+        }).bindPopup("<h3>Brand: <b>" + data[i].brand +"<b/><h/3><h4>Location: <b>" + data[i].loc_name +"<b/><h/4><br><p>Cost Center: "+ data[i].cost_center + "<p>Total Promo Sales: $"+ data[i].sum_promo_sales + "<h/4>").addTo(myMap2);
+      }
+    });
+
+
+
+
+// ---------------------------------------------------------------------------------------------
+
+// // The Chart.js doughnut visual - Acres Burned
+
+
+// var ctx = document.getElementById('doughChart').getContext('2d');
+// var doughnutChart = new Chart(ctx, {
+//     // The type of chart we want to create
+//     type: 'doughnut',
+
+//     // The data for our dataset
+//     data: {
+//         labels: ['0-1 acres', '1-5 acres', '5-10 acres', '10-100 acres', '100-1000 acres', '1000-5000 acres', '5000-10000 acres', '10000 +'],
+//         datasets: [{
+//             label: 'My dataset',
+//             backgroundColor: ['rgb(8, 8, 248)', 'rgb(8, 152, 248)','rgb(8, 248, 88)', 'rgb(33, 141, 6)',
+//             'rgb(8, 83, 1)', 'rgb(208, 255, 0)','rgb(255, 145, 0)', 'rgb(255, 0, 0)' ],
+//             borderColor: 'rgb(255, 255, 255)',
+//             data: [63290, 16279, 2450, 5046, 2127, 749, 223, 344]
+//         }]
+//     },
+//     // Configuration options go here
+//     options: {}
+
+// });
+
+
+
+
+
+// // ---------------------------------------------------------------------------------------------
+
+// //  Fire Cause Visualization
+
+// var ctx2 = document.getElementById('doughChart2').getContext('2d');
+//   var doughnutChart2 = new Chart(ctx2, {
+//     // The type of chart we want to create
+//     type: 'doughnut',
+//     // The data for our dataset
+//     data: {
+//         labels: ['0-Unknown', '1-Lightning', '2-Equipment Use', '3-Smoking', '4-Campfire', '5-Debris Burning', '6-Railroad', '7-Arson', '8-Children', '9-Miscellaneous'],
+//         datasets: [{
+//             label: 'My dataset',
+//             backgroundColor: ['#03071E', "#370617", "#6A040F", '#9D0208', "#D00000", "#DC2F02","#E85D04", "#F48C06", "#FAA307", "#FFBA08"],
+//             borderColor: 'rgb(255, 255, 255)',
+//             data: [18, 1091, 85, 2, 57, 15, 11, 94, 3, 394]
+//         }]
+//     },
+//     // Configuration options go here
+//     options: {}
+//   });
+
+//   // ---------------------------------------------------------------------------------------------
+
+// //  Total Acres by Year Visualization
+// var ctx3 = document.getElementById('doughChart3').getContext('2d');
+//   var doughnutChart3 = new Chart(ctx3, {
+//     // The type of chart we want to create
+//     type: 'doughnut',
+//     // The data for our dataset
+//     data: {
+//         labels: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
+//         datasets: [{
+//             label: 'My dataset',
+//             backgroundColor: ['#250066', "#390099", "#6C0079", '#850069', "#9E0059", "#FF0054","#FF2A2A", "#FF5400", "#FF8900", "#FFBD00"],
+//             borderColor: 'rgb(255, 255, 255)',
+//             data: [372059, 2069404, 4349173, 1510563, 952690, 2510226, 1324693, 2882366, 2565586, 770370]
+//         }]
+//     },
+//     // Configuration options go here
+//     options: {}
+//   });
+  
